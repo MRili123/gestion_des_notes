0166502ca28131b51515f702eddb717ba37c68d2
Younes <101892216+VirtuMind@users.noreply.github.com>
2025-01-27 17:59:31 +0100

    Fixed routes names

--- GestionNote/src/main/java/com/example/GestionNote/config/AuthHandler.java
+++ GestionNote/src/main/java/com/example/GestionNote/config/AuthHandler.java
@@ -22,9 +22,9 @@ public class AuthHandler implements AuthenticationSuccessHandler {
             Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
             if (roles.contains("ADMIN_USER")) {
-                response.sendRedirect("/users/home");
+                response.sendRedirect("/AdminUser/home");
             } else if (roles.contains("ADMIN_NOTES")) {
-                response.sendRedirect("/notes/home");
+                response.sendRedirect("/AdminNotes/home");
             } else if (roles.contains("ADMIN_SP")) {
                 response.sendRedirect("/AdminSp/home");
             } else {
